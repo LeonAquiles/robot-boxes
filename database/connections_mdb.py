dba48f688cde3a21efa48ba621d6a9cdc8ebb50a
LeonAquiles <98592543+LeonAquiles@users.noreply.github.com>
2022-01-31 14:37:25 -0300

    Add files via upload

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -1,7 +1,11 @@
-# ©PANDITHAN
 import pymongo
 
 from info import DATABASE_URI, DATABASE_NAME
+
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
 myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
 mycol = mydb['CONNECTION']   
@@ -32,7 +36,7 @@ async def add_connection(group_id, user_id):
             mycol.insert_one(data)
             return True
         except:
-            print('Some error occured!')
+            logger.exception('Some error occurred!', exc_info=True)
 
     else:
         try:
@@ -45,7 +49,7 @@ async def add_connection(group_id, user_id):
             )
             return True
         except:
-            print('Some error occured!')
+            logger.exception('Some error occurred!', exc_info=True)
 
         
 async def active_connection(user_id):
@@ -58,10 +62,7 @@ async def active_connection(user_id):
         return None
 
     group_id = query['active_group']
-    if group_id != None:
-        return int(group_id)
-    else:
-        return None
+    return int(group_id) if group_id != None else None
 
 
 async def all_connections(user_id):
@@ -127,6 +128,6 @@ async def delete_connection(user_id, group_id):
             )
         return True
     except Exception as e:
-        print(e)
+        logger.exception(f'Some error occurred! {e}', exc_info=True)
         return False
 
